68a447933c33a2ab42de29b85974bb8d1c416bd4
ROCK\KeL_U <luoke950708@gmail.com>
2018-01-18 22:50:15 +0800

    augment small image data set

--- image_preprocess.py
+++ image_preprocess.py
@@ -37,8 +37,8 @@ def resizeImg(**args):
     :return:
 
     example：
-    ori_img = "D:/ori/cat_origin.jpg"
-    dst_img = "D:/dst/cat_resize.jpg"
+    ori_img = "D:/ori/img_origin.jpg"
+    dst_img = "D:/dst/img_resize.jpg"
     dst_w = 94
     dst_h = 94
     save_q = 100
@@ -81,28 +81,100 @@ def resizeImg(**args):
     img.resize((newWidth, newHeight), Image.ANTIALIAS).save(arg['dst_img'], quality=arg['save_q'])
 
 
-image_datagen = ImageDataGenerator(
-    featurewise_center=True,
-    featurewise_std_normalization=True,
-    rotation_range=15,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True,
-    vertical_flip=True,
-    fill_mode='reflect')
+def generateImage(img_dir, save_dir, prefix, gen_num=1, save_format='jpeg'):
+    '''
+    :param img_dir: the directory of images need to be augmented
+    :param save_dir: the directory of augmented images to be stored
+    :param prefix: prefix of the name of augmented images
+    :param save_format: format of augmented images. 'jepg' or 'png'
+    :return:
+    '''
+    image_datagen = ImageDataGenerator(
+        featurewise_center=True,
+        featurewise_std_normalization=True,
+        rotation_range=15,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        vertical_flip=True,
+        fill_mode='reflect')
 
-img_dir = "D:/Rename/augment/cat.jpg"
-new_img_dir = "D:/Rename/augment/newgen"
-img = load_img(img_dir)
-x = img_to_array(img)
-x = x.reshape((1,) + x.shape)
+    if os.path.isdir(save_dir):
+        pass
+    else:
+        os.makedirs(save_dir)
 
-image_datagen.fit(x, augment=True)
-i = 0
-for batch in image_datagen.flow(x, batch_size=5,
-                                save_to_dir=new_img_dir, save_prefix='dog', save_format='jpeg'):
-    i += 1
-    if i > 1:
-        break
+    for file in os.listdir(img_dir):
+        if os.path.isdir(file):
+            continue
+        else:
+
+            image_dir = os.path.join(img_dir, file)
+            img = load_img(image_dir)
+            x = img_to_array(img)
+            x = x.reshape((1,) + x.shape)
+
+            image_datagen.fit(x, augment=True)
+            i = 0
+            for _ in image_datagen.flow(x, save_to_dir=save_dir, save_prefix=prefix, save_format=save_format):
+                i += 1
+                if i >= gen_num:
+                    break
+
+
+def clipResizeImg(**kwargs):
+    '''
+    :param kwargs:
+    :return:
+
+    example：
+    ori_img = "D:/ori/img_origin.jpg"
+    dst_img = "D:/dst/img_resize.jpg"
+    dst_w = 94
+    dst_h = 94
+    save_q = 100
+
+    resizeImg(ori_img=ori_img,dst_img=dst_img,dst_w=dst_w,dst_h=dst_h,save_q = save_q)
+    '''
+    args_key = {'ori_img': '', 'dst_img': '', 'dst_w': '', 'dst_h': '', 'save_q': 75}
+    arg = {}
+    for key in args_key:
+        if key in kwargs:
+            arg[key] = kwargs[key]
+
+    im = Image.open(arg['ori_img'])
+    ori_w, ori_h = im.size
+
+    dst_scale = float(arg['dst_h']) / arg['dst_w']  # 目标高宽比
+    ori_scale = float(ori_h) / ori_w  # 原高宽比
+
+    if ori_scale >= dst_scale:
+        # 过高
+        width = ori_w
+        height = int(width * dst_scale)
+
+        x = 0
+        y = (ori_h - height) / 3
+
+    else:
+        # 过宽
+        height = ori_h
+        width = int(height * dst_scale)
+
+        x = (ori_w - width) / 2
+        y = 0
+
+    # 裁剪
+    box = (x, y, width + x, height + y)
+    # 这里的参数可以这么认为：从某图的(x,y)坐标开始截，截到(width+x,height+y)坐标
+    # 所包围的图像，crop方法与php中的imagecopy方法大为不一样
+    newIm = im.crop(box)
+    im = None
+
+    # 压缩
+    ratio = float(arg['dst_w']) / width
+    newWidth = int(width * ratio)
+    newHeight = int(height * ratio)
+    newIm.resize((newWidth, newHeight), Image.ANTIALIAS).save(arg['dst_img'], quality=arg['save_q'])
---
+++ input_images.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/1/18 14:39
+# @File    : input_images.py
+# @Author  : NusLuoKe
+
+import os
+from image_preprocess import resizeImg, rename_files, generateImage, clipResizeImg
+
+image_dir = 'T:/data_augmentation_demo/original_images'
+resized_img_dir = 'T:/data_augmentation_demo/resized_images'
+clipResized_img_dir = 'T:/data_augmentation_demo/clipResized_img_dir_images'
+
+if not os.path.isdir(resized_img_dir):
+    os.makedirs(resized_img_dir)
+
+if not os.path.isdir(clipResized_img_dir):
+    os.makedirs(clipResized_img_dir)
+
+for image in os.listdir(image_dir):
+    if not os.path.isdir(image):
+        ori_img = os.path.join(image_dir, image)
+        dst_img = os.path.join(resized_img_dir, image)
+        dst_w = 128
+        dst_h = 128
+        save_q = 75
+        resizeImg(ori_img=ori_img, dst_img=dst_img, dst_w=dst_w, dst_h=dst_h, save_q=save_q)
+
+for resi_image in os.listdir(resized_img_dir):
+    if not os.path.isdir(resi_image):
+        ori_img = os.path.join(resized_img_dir, resi_image)
+        dst_img = os.path.join(clipResized_img_dir, resi_image)
+        dst_w = 128
+        dst_h = 128
+        save_q = 75
+        clipResizeImg(ori_img=ori_img, dst_img=dst_img, dst_w=dst_w, dst_h=dst_h, save_q=save_q)
+
+augmented_img_dir = 'T:/data_augmentation_demo/augmented_images'
+if not os.path.isdir(augmented_img_dir):
+    os.makedirs(augmented_img_dir)
+generateImage(img_dir=clipResized_img_dir, save_dir=augmented_img_dir, prefix='pet', gen_num=5)
+rename_files(file_dir=augmented_img_dir, new_prefix='pet')
